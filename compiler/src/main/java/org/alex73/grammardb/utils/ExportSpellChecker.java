526cc35fb959a267aa56a0cd4183c1ed55038812
Aleś Bułojčyk <alex73mail@gmail.com>
2026-07-01 14:58:57 +0300

    Выпраўленні экспарта

--- compiler/src/main/java/org/alex73/grammardb/utils/ExportSpellChecker.java
+++ compiler/src/main/java/org/alex73/grammardb/utils/ExportSpellChecker.java
@@ -10,7 +10,6 @@ import java.util.stream.Collectors;
 
 import org.alex73.grammardb.FormsReadyFilter;
 import org.alex73.grammardb.GrammarDB2;
-import org.alex73.grammardb.SetUtils;
 import org.alex73.grammardb.StressUtils;
 import org.alex73.grammardb.structures.Form;
 import org.alex73.grammardb.structures.Variant;
@@ -34,10 +33,12 @@ public class ExportSpellChecker extends AbstractMojo {
 
     @Parameter(property = "xmlDataDir", required = true)
     String xmlDataDir;
-    @Parameter(property = "textOutputFile", required = true)
-    String textOutputFile;
-    @Parameter(property = "textOutputFileWithStress", required = true)
-    String textOutputFileWithStress;
+    @Parameter(property = "textOutputFile2008", required = true)
+    String textOutputFile2008;
+    @Parameter(property = "textOutputFile2008withStress", required = true)
+    String textOutputFile2008withStress;
+    @Parameter(property = "textOutputFileAll", required = false)
+    String textOutputFileAll;
 
     public void execute() throws MojoExecutionException, MojoFailureException {
         try {
@@ -50,59 +51,52 @@ public class ExportSpellChecker extends AbstractMojo {
     private void run() throws Exception {
         GrammarDB2 db = GrammarDB2.initializeFromDir(Path.of(xmlDataDir).toAbsolutePath().toString());
 
-        List<String> list2008 = new ArrayList<>();
-        List<String> temp1 = new ArrayList<>();
-        List<String> temp2 = new ArrayList<>();
-        List<String> temp3 = new ArrayList<>();
+        List<String> list2008stressed = new ArrayList<>();
+        List<String> allWordsStressed = new ArrayList<>();
 
         System.out.println("Збор слоў для праверкі правапісу...");
         db.getAllParadigms().forEach(p -> {
-            List<String> result = new ArrayList<>();
             for (Variant v : p.getVariant()) {
-                List<Form> fs1 = FormsReadyFilter.getAcceptedForms(FormsReadyFilter.MODE.SPELL, p, v);
-                if (fs1 == null) {
-                    continue;
+                for (Form f : v.getForm()) {
+                    if (!f.getValue().isEmpty()) {
+                        allWordsStressed.add(f.getValue().replace('+', '\u0301').replace(GrammarDB2.pravilny_apostraf, '\''));
+                    }
                 }
-                if (!fs1.isEmpty()) {
-                    fs1.forEach(f -> result.add(f.getValue()));
-                    result.add("");
-                }
-                String tag = SetUtils.tag(p, v);
-                for (Form f : fs1) {
-                    temp1.add(StressUtils.unstress(f.getValue()) + "\t" + StressUtils.unstress(v.getLemma()) + "\t" + tag.charAt(0));
-                    temp2.add(StressUtils.unstress(f.getValue()).replace(GrammarDB2.pravilny_apostraf, '\''));
-                    temp3.add(f.getValue().replace('+', '\u0301').replace(GrammarDB2.pravilny_apostraf, '\''));
+                List<Form> fsSpell = FormsReadyFilter.getAcceptedForms(FormsReadyFilter.MODE.SPELL, p, v);
+                if (fsSpell != null) {
+                    for (Form f : fsSpell) {
+                        list2008stressed.add(f.getValue().replace('+', '\u0301').replace(GrammarDB2.pravilny_apostraf, '\''));
+                    }
                 }
             }
-            list2008.addAll(result);
         });
 
         System.out.println("Апрацоўка спісаў...");
-        duplicateU(list2008, false);
-        duplicateU(temp1, true);
-        duplicateU(temp2, true);
+        duplicateU(list2008stressed);
+        duplicateU(allWordsStressed);
         System.out.println("Сартаванне спісаў...");
 
-        List<String> list2008uniq = temp2.stream().sorted(BE).distinct().collect(Collectors.toList());
-        List<String> list2008uniqStress = temp3.stream().sorted(BE).distinct().collect(Collectors.toList());
+        List<String> list2008uniqUnstessed = list2008stressed.stream().map(w -> StressUtils.unstress(w)).sorted(BE).distinct().collect(Collectors.toList());
+        List<String> list2008uniqStressed = list2008stressed.stream().sorted(BE).distinct().collect(Collectors.toList());
+        List<String> allWordsUniqUnstessed = allWordsStressed.stream().map(w -> StressUtils.unstress(w)).sorted(BE).distinct().collect(Collectors.toList());
 
-        Path out = Path.of(textOutputFile);
-        Path outStress = Path.of(textOutputFileWithStress);
+        Path out = Path.of(textOutputFile2008);
+        Path outStress = Path.of(textOutputFile2008withStress);
+        Path all = Path.of(textOutputFileAll);
         Files.createDirectories(out.getParent());
         Files.createDirectories(outStress.getParent());
-        Files.write(out, list2008uniq);
-        Files.write(outStress, list2008uniqStress);
+        Files.createDirectories(all.getParent());
+        Files.write(out, list2008uniqUnstessed);
+        Files.write(outStress, list2008uniqStressed);
+        Files.write(all, allWordsUniqUnstessed);
     }
 
-    static void duplicateU(List<String> words, boolean addToEnd) {
+    static void duplicateU(List<String> words) {
         for (int i = 0; i < words.size(); i++) {
-            if (words.get(i).startsWith("у")) {
+            if (words.get(i).startsWith("у") && !words.get(i).startsWith("у" + GrammarDB2.pravilny_nacisk)) {
+                // пачынаецца з ненаціскнога "у", дадаем варыянт з "ў"
                 String neww = "ў" + words.get(i).substring(1);
-                if (addToEnd) {
-                    words.add(neww);
-                } else {
-                    words.add(i + 1, neww);
-                }
+                words.add(neww);
             }
         }
     }
